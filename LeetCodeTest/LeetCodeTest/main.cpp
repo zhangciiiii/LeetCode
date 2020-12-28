6f50b2b83123a99f63d3864cd23e0f306254e7a8
ZhangChi <zhangchi@ZhangChideMacBook-Pro.local>
2020-12-28 08:40:42 +0800

    update

---
+++ LeetCodeTest/LeetCodeTest/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  LeetCodeTest
+//
+//  Created by ZhangChi on 2020/12/28.
+//
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> results;
+        
+        results.push_back(nums);
+        return results;
+    }
+};
+
+int main() {
+    // insert code here...
+    cout<< "Hello, World!\n";
+    return 0;
+}
--- T0007/README.md
+++
@@ -1,5 +0,0 @@
-# T7 整数翻转
-
-INT_MAX 是int型的最大值，INT_MIN 是int型的最小值，
-
-INT_MAX + 1 = INT_MIN， INT_MIN - 1 = INT_MAX
